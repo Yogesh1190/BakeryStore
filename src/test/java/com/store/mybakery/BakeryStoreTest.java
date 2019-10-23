7a1063876c307cf3b9dcd6fcbfbdc658dd95f73e
Yogesh Shisode <yogesh.shisode@infintus.com>
2019-10-23 17:16:46 +0530

    Base Project

---
+++ src/test/java/com/store/mybakery/BakeryStoreTest.java
@@ -0,0 +1,20 @@
+package com.store.mybakery;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class BakeryStoreTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
