909b979566ab8d62403e0ac9f799a53141d00af2
Yogesh Shisode <yogesh.shisode@infintus.com>
2019-10-23 19:49:08 +0530

    Source code updated with calculate cost functionality

--- src/main/java/com/store/mybakery/impl/CheckoutOrder.java
+++ src/main/java/com/store/mybakery/impl/CheckoutOrder.java
@@ -1,5 +1,12 @@
 package com.store.mybakery.impl;
 
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 import com.store.mybakery.model.ProductInput;
@@ -13,9 +20,50 @@ import com.store.mybakery.model.ProductInput;
 public class CheckoutOrder {
 	
 	public void calculate(JSONObject products, ProductInput input){
+		Map<Integer, Integer> response = new HashMap<Integer, Integer>();
 		
-		System.out.println("Welcome to checkout order funtion");
-		System.out.println("Input request is : " + input.getCode() + " -- " + input.getQuantity());
-		System.out.println("Available Data is : " + products);
+		Map<Integer, Double> hashMap = getPackDetails(products, input);
+		int inputQty = input.getQuantity();
+		
+		for(Map.Entry<Integer, Double> packs : hashMap.entrySet()){
+			if((inputQty / packs.getKey()) > 0){
+				int packQty = packs.getKey();
+				response.put(packQty, inputQty / packQty);
+				inputQty = inputQty % packQty;
+			}
+		}
+		
+		for(Map.Entry<Integer, Integer> resp : response.entrySet()){
+			System.out.println(resp.getKey() + " -- " + resp.getValue());
+		}
+	}
+	
+	public Map<Integer, Double> getPackDetails(JSONObject products, ProductInput input){
+		JSONArray items = (JSONArray) products.get("products");
+		Iterator<JSONObject> iterator = items.iterator();
+		
+		Map<Integer, Double> hashMap = new TreeMap<Integer, Double>(new Comparator<Integer>() {
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				return o2.compareTo(o1);
+			}
+		});
+		
+		while(iterator.hasNext()){
+			JSONObject test = iterator.next();
+			if(String.valueOf(test.get("code")).equalsIgnoreCase(input.getCode())){
+				JSONArray packs = (JSONArray) test.get("pack");
+				Iterator<JSONObject> packsIterator = packs.iterator();
+				while(packsIterator.hasNext()){
+					JSONObject packDetails = packsIterator.next();
+					int quantity = Integer.parseInt(packDetails.get("quantity").toString());
+					double cost = Double.parseDouble(packDetails.get("cost").toString());
+					hashMap.put(quantity, cost);
+				}
+				break;
+			}
+		}
+		
+		return hashMap;
 	}
 }
